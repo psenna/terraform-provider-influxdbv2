c23bf48858c030133a4839051cb09ce0dacb5525
psenna <pedrosennapsc@gmail.com>
2024-03-10 22:29:58 +0000

    Release build

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,12 @@
-provider "scaffolding" {
-  # example configuration here
+terraform {
+  required_providers {
+    influxdbv2 = {
+      source = "registry.terraform.io/psenna/influxdbv2"
+    }
+  }
+}
+
+provider "influxdbv2" {
+    host = "http://influxdb:8086"
+    api_key = "V75L9W05AABQBCACF6F8CVDJTPFEXA"
 }
